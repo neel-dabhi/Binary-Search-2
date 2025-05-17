fac19a7fd79b31c0247b23ad2dab7ae2e4d05cb2
neel-dabhi <yoursdabhineelkanth@gmail.com>
2025-05-17 19:55:32 -0300

    completed binary search 2

---
+++ findMinRotatedSortedArray.java
@@ -0,0 +1,37 @@
+// Time Complexity : O(log N)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : YES
+// Any problem you faced while coding this :
+
+class findMinRotatedSortedArray {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[low] <= nums[high]) {
+                return nums[low];
+            }
+
+            if ((mid != 0 && nums[mid] < nums[mid - 1]) &&
+                    (mid != nums.length -1 && nums[mid] < nums[mid + 1])) {
+                return nums[mid];
+            }
+
+            if (nums[mid + 1] < nums[mid]) {
+                return nums[mid + 1];
+            }
+
+            if (nums[low] <= nums[mid]) {
+                // left is sorted
+                low = mid + 1;
+            } else {
+                // right is sorted
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
